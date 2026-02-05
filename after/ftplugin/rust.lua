52f350ecc406c1be21914f3a7d72721e56554f8b
r-mercer <riley.mercer4@gmail.com>
2026-02-05 16:55:49 +0800

    update packs and also resolve annoying vim global var issue

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>gra', function()
   vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
   -- or vim.lsp.buf.codeAction() if you don't want grouping.
 end, { silent = true, buffer = bufnr })
+
 vim.keymap.set(
   'n',
   'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
