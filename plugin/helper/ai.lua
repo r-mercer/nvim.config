dba637417e0f1e757d81caa421adde425d8edb7e
r-mercer <riley.mercer4@gmail.com>
2026-02-23 07:17:54 +0800

    Updates to nvim

--- plugin/helper/ai.lua
+++ plugin/helper/ai.lua
@@ -2,10 +2,42 @@ vim.pack.add {
   { src = 'https://github.com/nvim-lua/plenary.nvim' },
   -- { src = 'https://github.com/nvim-lua/plenary.nvim' },
   { src = 'https://github.com/MunifTanjim/nui.nvim' },
-  { src = 'https://github.com/yetone/avante.nvim' },
+  -- { src = 'https://github.com/yetone/avante.nvim' },
   { src = 'https://github.com/zbirenbaum/copilot.lua' },
   -- { src = 'https://github.com/zbirenbaum/copilot.lua' },
 }
 
-require('copilot').setup()
-require('avante').setup()
+require('copilot').setup {
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = '[[',
+      jump_next = ']]',
+      accept = '<CR>',
+      refresh = 'gr',
+      open = '<M-CR>',
+    },
+    layout = {
+      position = 'bottom', -- | top | left | right | bottom |
+      ratio = 0.4,
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    hide_during_completion = true,
+    debounce = 15,
+    trigger_on_accept = true,
+    keymap = {
+      accept = '<A-l>',
+      accept_word = false,
+      accept_line = false,
+      next = '<A-]>',
+      prev = '<A-[>',
+      dismiss = '<C-]>',
+      toggle_auto_trigger = false,
+    },
+  },
+}
+-- require('avante').setup()
--- plugin/helper/blink.lua
+++ plugin/helper/blink.lua
@@ -4,7 +4,7 @@ vim.pack.add {
     version = vim.version.range '1.*',
   },
   'https://github.com/rafamadriz/friendly-snippets',
-  'https://github.com/Kaiser-Yang/blink-cmp-avante',
+  'https://github.com/giuxtaposition/blink-cmp-copilot',
   'https://github.com/L3MON4D3/LuaSnip',
 }
 
@@ -22,14 +22,13 @@ require('blink.cmp').setup {
   ghost_text = { enabled = true },
 
   sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer', 'avante' },
+    default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
     providers = {
-      avante = {
-        module = 'blink-cmp-avante',
-        name = 'Avante',
-        opts = {
-          -- options for blink-cmp-avante
-        },
+      copilot = {
+        name = 'copilot',
+        module = 'blink-cmp-copilot',
+        score_offset = 100,
+        async = true,
       },
     },
   },
